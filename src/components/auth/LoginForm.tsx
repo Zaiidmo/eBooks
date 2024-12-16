9a3e2390f9ef0a1ab92e6fa42dbde16822c04ebc
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-12-16 16:19:12 +0100

    Update Login Loading Styles

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -70,7 +70,7 @@ export default function LoginForm() {
 
   if(loading) {
     return (
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center items-center h-fit">
         <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900 dark:border-gray-100"></div>
       </div>
     );
